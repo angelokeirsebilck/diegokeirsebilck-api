00509370cadd8ac550acadb0252b3ad18475c632
Angelo Keirsebilck <angelo.keirsebilck1@telenet.be>
2021-05-28 20:12:44 +0200

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '30752fc5e63e0017e4a350ead1728175'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5062fd3c-197b-4cfe-8113-d570dc532b24'
+};
